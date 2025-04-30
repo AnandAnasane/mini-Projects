52408414459c9cec8e37e2f263fb433a41a57e6c
AnandAnasane <aanand08a@gmail.com>
2025-04-30 14:17:11 +0530

    update

---
+++ src/pages/todo/todo.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from '../../model/model';
+
+@Component({
+  selector: 'app-todo',
+  standalone: true,
+  imports: [],
+  templateUrl: './todo.component.html',
+  styleUrl: './todo.component.css'
+})
+export class TodoComponent  implements OnInit
+{
+
+  ngOnInit(): void {
+    
+  }
+
+  
+
+
+}
